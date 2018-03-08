a6eae006c43b5861faf0830360debb69668376d9
Don Peter C. Atunalu <patunalu@yahoo.com>
2018-03-08 12:31:11 +0200

    Added release docker image and config
    Signed-off-by: Don Peter C. Atunalu <patunalu@yahoo.com>

---
+++ docker/release/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Install dependency
+echo "Installing node dependencies..."
+yarn install --offline --pure-lockfile
+#echo "Removing cache dependencies"
+#rm -rf ./npm-packages-offline-cache
+#Run CMD parameter as PID 0
+exec $@
